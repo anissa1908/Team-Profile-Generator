6e117cfac99841cd73064e668353300529a59501
Anissa Shanks <anissa.shanks@gmail.com>
2020-08-23 16:17:22 -0500

    added class for employee and return functions on employee.js file

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,12 +1,38 @@
 // TODO: Write code to define and export the Employee class
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// const render = require("./lib/htmlRenderer");
+
+
+class Employee {
+    constructor (name, id, email) {
+        this.name = name;
+        this.email = email;
+        this.id = id;
+        this.role = role;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getID() {
+        return this.id;
+    }
+
+    getEmail() {
+        return this.email;
+    }
+
+    getRole() {
+        return this.Employee;
+    }
+}
